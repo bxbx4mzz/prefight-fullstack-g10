1a5207da452ee5872ffcf8e99c43790fb533d008
tie <tie.teiroom1234@gmail.com>
2026-07-29 17:09:38 +0700

    fix: complete register route logic

--- pf-backend/db/schema.ts
+++ pf-backend/db/schema.ts
@@ -6,14 +6,6 @@ import {
   boolean,
 } from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable("users", {
-  id: uuid("id").primaryKey().defaultRandom(), // รหัส user แบบ UUID สุ่ม
-  name: varchar("name", { length: 100 }).notNull(),
-  email: varchar("email", { length: 150 }).notNull().unique(), // ห้ามซ้ำ
-  password: varchar("password", { length: 255 }).notNull(), // เก็บ hash ไม่ใช่ plain text
-  createdAt: timestamp("created_at").defaultNow().notNull(),
-});
-
 export const todoTable = pgTable("todo", {
   id: uuid("id").primaryKey().defaultRandom(),
   todoText: varchar("todo_text", { length: 255 }).notNull(),
